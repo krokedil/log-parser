037d55427fab870959490fbe28ce4d44ffcc49c7
MichaelBengtsson <michael@krokedil.se>
2024-05-13 09:07:18 +0200

    Implement new class and ulrichsg/getopt-php

--- index.php
+++ index.php
@@ -1,91 +1,64 @@
-<?php // phpcs:disable
-/**
- * Loop all files in the logs folder and find the passed term.
- *
- * @param string $term The term to search for.
- *
- * @return array
- */
-function find_files_with_transaction_id( $term ) {
-	echo "Searching for term: $term\n";
-	$files  = glob( __DIR__ . '/logs/*.log' );
-	$result = array();
-	foreach ( $files as $file ) {
-		$content = file_get_contents( $file );
-		if ( strpos( $content, $term ) !== false ) {
-			echo "Found term $term in file: $file\n";
-			$result[] = $file;
-		}
-	}
-	return $result;
+<?php
+require_once 'vendor/autoload.php';
+
+use GetOpt\GetOpt;
+use GetOpt\Command;
+use GetOpt\Operand;
+use GetOpt\Option;
+
+$termsOperand = Operand::create( 'terms', Operand::MULTIPLE );
+
+$getopt = new Getopt();
+
+$getopt->addCommand(
+	Command::create(
+		'parse',
+		'parse',
+		array(
+			Option::create( 'l', 'logs', Getopt::REQUIRED_ARGUMENT )->setDescription( 'Logs folder path' ),
+			Option::create( 'o', 'output', Getopt::REQUIRED_ARGUMENT )->setDescription( 'Output folder path' ),
+			Option::create( 'v', 'verbose', Getopt::NO_ARGUMENT )->setDescription( 'Verbose mode' ),
+			Option::create( 'i', 'inclusive', Getopt::NO_ARGUMENT )->setDescription( 'Inclusive mode, will only get lines that contain all the terms passed.' ),
+			Option::create( 'h', 'help', Getopt::NO_ARGUMENT )->setDescription( 'Show help text' ),
+		)
+	)
+	->addOperand( $termsOperand )
+	->setDescription( "Parse the logs and get all rows that contain either any or all of the terms.\nExample:\n   php index.php parse term1 term2 term3" )
+	->setShortDescription( 'Parse logs' )
+);
+
+try {
+	$getopt->process();
+} catch ( \GetOpt\ArgumentException $exception ) {
+	echo $exception->getMessage() . PHP_EOL;
 }
 
-/**
- * Parse the log files and return each row that contains the term.
- *
- * @param string $term The term to search for.
- *
- * @return array
- */
-function parse_log( $term ) {
-	$files  = find_files_with_transaction_id( $term );
-	$result = array();
-	foreach ( $files as $file ) {
-		$result[] = "From file: $file";
-		$content  = file_get_contents( $file );
-		$rows     = explode( "\n", $content );
-		foreach ( $rows as $row ) {
-			if ( strpos( $row, $term ) !== false ) {
-				$result[] = $row;
-			}
-		}
-	}
-	return $result;
+$command = $getopt->getCommand();
+if ( ! $command ) {
+	$getopt->getHelpText();
+} else {
+	call_user_func( $command->getHandler(), $getopt->getOptions(), $getopt->getOperands() );
 }
 
-/**
- * Save all rows to a new file in the output folder.
- *
- * @param array $terms The arguments passed to the script.
- *
- * @return string
- */
-function save_output( $terms ) {
-	$rows = array();
-	foreach ( $terms as $term ) {
-		$rows = array_unique( array_merge( $rows, parse_log( $term ) ) );
+function parse( $flags, $terms ) {
+	global $getopt;
+	$help = isset( $flags['help'] );
+
+	if ( $help ) {
+		echo $getopt->getHelpText();
+		return;
 	}
 
-	// Create the output folder if it doesn't exist.
-	if ( ! is_dir( 'output' ) ) {
-		mkdir( 'output' );
+	$verbose   = isset( $flags['verbose'] );
+	$inclusive = isset( $flags['inclusive'] );
+
+	if ( $verbose ) {
+		echo "Verbose mode\n";
 	}
-	$filename = implode( '-', $terms );
-	// Strip any unwanted characters from the filename.
-	$filename = preg_replace( '/[^a-z0-9-]/', '', strtolower( $filename ) );
 
-	$file = __DIR__ . '/output/' . $filename . '.log';
+	$logs_dir   = $flags['logs'] ?? __DIR__ . '/logs';
+	$output_dir = $flags['output'] ?? __DIR__ . '/output';
 
-	// Save the rows to the file.
-	file_put_contents( $file, implode( "\n", $rows ) );
-
-	return $file;
+	$log_parser = new \Krokedil\LogParser\LogParser( $logs_dir, $output_dir, $terms, $inclusive, $verbose );
+	$log_parser->parse();
 }
-
-/**
- * Run the script.
- *
- * @param array $terms The arguments passed to the script.
- *
- * @return void
- */
-function run( $terms ) {
-	// Unset the filename from the arguments.
-	unset( $terms[0] );
-
-	$file = save_output( $terms );
-	echo "The output has been saved to: $file\n";
-}
-
-// Run the script.
-run( $argv );
